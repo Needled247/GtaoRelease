bcb7668ec942ed92a99e26aedfbc7d66ab8740cb
Hao <publicvoidjh@gmail.com>
2013-11-22 17:32:14 +0800

    2013-11-22

--- src/servlet/IndexInit.java
+++ src/servlet/IndexInit.java
@@ -1,11 +1,16 @@
 package servlet;
 
+import db.ConnPool;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * 主页初始化。
@@ -23,6 +28,54 @@ public class IndexInit extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/json;charset=GBK");
         PrintWriter out = response.getWriter();
-        //TODO:查询三种资源，每种5个，拼接成json字符串。
+        String filmInitQuery = "SELECT ID,FILENAME FROM rl_film WHERE STATUS='999'";
+        String softInitQuery = "SELECT ID,FILENAME FROM rl_software WHERE STATUS='999'";
+        String gameInitQuery = "SELECT ID,FILENAME FROM rl_game WHERE STATUS='999'";
+        //拼接JSON字符串
+        String rtnJson = "{\"film\":"+this.getShareInfoJson(filmInitQuery)
+                +"\"soft\":"+this.getShareInfoJson(softInitQuery)
+                +"\"game\":"+this.getShareInfoJson(gameInitQuery);
+        if(rtnJson.length()>0){
+            rtnJson = rtnJson.substring(0,rtnJson.lastIndexOf(","));
+        }
+        rtnJson += "}";
+        //打印
+        out.print(rtnJson);
+        out.flush();
+        out.close();
+    }
+
+    protected String getShareInfoJson(String sql){
+        ConnPool cp = new ConnPool();
+        ResultSet rs = null;
+        StringBuilder info = new StringBuilder();
+        info.append("[");
+        PreparedStatement pstmt = null;
+        try{
+            pstmt = cp.getConn().prepareStatement(sql);
+            rs = pstmt.executeQuery();
+            while (rs.next()){
+                info.append("{\"id\":\""+rs.getInt(1)+"\",")
+                    .append("\"name\":\""+rs.getString(2)+"\"},");
+            }
+            if(info.length()!=0){
+                info.deleteCharAt(info.lastIndexOf(","));
+            }
+            info.append("],");
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            if(pstmt!=null){
+                try{
+                    pstmt.close();
+                }
+                catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return info.toString();
     }
 }
